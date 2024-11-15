191820c1934aa60975880c5810af1e0df9d76919
ummy <chachioumaima4@gmail.com>
2024-11-16 00:40:12 +0100

    Your commit message

--- ft_hhhh.c
+++ ft_hhhh.c
@@ -15,36 +15,39 @@
 int	ft_phex(uintptr_t num, char form)
 {
 	char	*b;
+	int		calc;
 
+	calc = 0;
 	if (form == 'x')
 		b = "0123456789abcdef";
 	else
 		b = "0123456789ABCDEF";
 	if (num >= 16)
 	{
-		ft_phex(num / 16, form);
+		calc += ft_phex(num / 16, form);
 	}
-	ft_putchar(b[num % 16]);
+	calc += ft_putchar(b[num % 16]);
+	return (calc);
 }
 
 int	ft_putp(void *p)
 {
 	if (!p)
 	{
-		return(write(1, "(nil)", 5));
+		return (write(1, "(nil)", 5));
 	}
-	return(write(1, "0x", 2));
-	ft_phex((uintptr_t)p, 'x');
+	write(1, "0x", 2);
+	return (2 + ft_phex((uintptr_t)p, 'x'));
 }
 
 int	ft_print_pr(void)
 {
-	return(write(1, "%", 1));
+	return (write(1, "%", 1));
 }
 
 size_t	ft_strlen(const char *str)
 {
-	size_t	tol;
+	size_t tol;
 
 	tol = 0;
 	while (str[tol] != '\0')
