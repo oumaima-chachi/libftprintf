6b61534fb8afc0075a222095573f1b3eab2278bb
ummy <chachioumaima4@gmail.com>
2024-11-20 09:43:59 +0100

    seconnd

--- main.c
+++ main.c
@@ -1,35 +1,37 @@
-#include "ft_printf.h"
-#include <stdio.h>
+// #include "ft_printf.h"
+// #include <stdio.h>
 
-int	main()
-{
-	int	num;
+// int	main()
+// {
+// 	// int	num;
 
-	// Test char
-	ft_printf("%c\n", 'A');  // A
-	ft_printf("%c\n", '\n'); // newline
-	// Test s
-	ft_printf("%s\n", "Hello, World!"); // Hello, World!
-	ft_printf("%s\n", NULL); //  (null)
-	// Test p
-	num = 42;
-	ft_printf("%p\n", &num); //  0x...chihaja
-	// Test int
-	ft_printf("%d\n", 42);          // 42
-	ft_printf("%d\n", -42);         //-42
-	ft_printf("%d\n", 2147483647);  // 2147483647
-	ft_printf("%d\n", -2147483648); // -2147483648
-	// Test unsigned
-	ft_printf("%u\n", 42);         // 42
-	ft_printf("%u\n", -42);        // 4294967254
-	ft_printf("%u\n", 4294967295); // 4294967295
-	// Test hexa
-	ft_printf("%x\n", 255); // ff
-	ft_printf("%X\n", 255); // FF
-	// Test per
-	ft_printf("%%\n"); //  %
-	// Mixed
-	ft_printf("salam ana  %s, endi %d chofo had l f chhal zwiwna hiya o khatha %x\n", "oumaima", 20,
-		255);
-
-}
+// 	// // Test char
+// 	// ft_printf("%c\n", 'A');  // A
+// 	// ft_printf("%c\n", '\n'); // newline
+// 	// // Test s
+// 	// ft_printf("%s\n", "Hello, World!"); // Hello, World!
+// 	// ft_printf("%s\n", NULL); //  (null)
+// 	// // Test p
+// 	// num = 42;
+// 	// ft_printf("%p\n", &num); //  0x...chihaja
+// 	// // Test int
+// 	// ft_printf("%d\n", 42);          // 42
+// 	// ft_printf("%d\n", -42);         //-42
+// 	// ft_printf("%d\n", 2147483647);  // 2147483647
+// 	// ft_printf("%d\n", -2147483648); // -2147483648
+// 	// // Test unsigned
+// 	// ft_printf("%u\n", 42);         // 42
+// 	// ft_printf("%u\n", -42);        // 4294967254
+// 	// ft_printf("%u\n", 4294967295); // 4294967295
+// 	// // Test hexa
+// 	// ft_printf("%x\n", 255); // ff
+// 	// ft_printf("%X\n", 255); // FF
+// 	// // Test per
+// 	// ft_printf("%%\n"); //  
+// 	// // Mixed
+// 	// ft_printf("salam ana  %s, endi %d chofo had l f chhal zwiwna hiya o khatha %x\n", "oumaima", 20,
+// 	// 	255);
+	
+// 	printf("Formatage : |%10d| |%-10d|\n", 123, 123);
+//     ft_printf("Formatage : |%10d| |%-10d|\n", 123, 123);
+// }
