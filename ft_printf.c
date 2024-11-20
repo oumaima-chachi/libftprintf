2d681803093e39ad3165fe6a41e727c08ac73aaf
ummy <chachioumaima4@gmail.com>
2024-11-20 09:43:59 +0100

    refactor: restructure ft_printf and test main

--- ft_printf.c
+++ ft_printf.c
@@ -6,16 +6,38 @@
 /*   By: ochachi <ochachi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 14:48:27 by ochachi           #+#    #+#             */
-/*   Updated: 2024/11/15 09:02:16 by ochachi          ###   ########.fr       */
+/*   Updated: 2024/11/17 01:13:31 by ochachi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "ft_printf.h"
+
+static int	h_specificateurs(char spec, va_list args)
+{
+	if (spec == 'c')
+		return (ft_putchar(va_arg(args, int)));
+	if (spec == 's')
+		return (ft_putstr(va_arg(args, char *)));
+	if (spec == 'p')
+		return (ft_putp(va_arg(args, void *)));
+	if (spec == 'd' || spec == 'i')
+		return (ft_putnbr(va_arg(args, int)));
+	if (spec == 'u')
+		return (ft_putunbr(va_arg(args, unsigned int)));
+	if (spec == 'x')
+		return (ft_phex(va_arg(args, unsigned int), 'x'));
+	if (spec == 'X')
+		return (ft_phex(va_arg(args, unsigned int), 'X'));
+	if (spec == '%')
+		return (ft_print_pr());
+	return (0);
+}
 
 int	ft_printf(const char *format, ...)
 {
-	va_list args;
-	int calc;
-	int i;
+	va_list	args;
+	int		calc;
+	int		i;
 
 	calc = 0;
 	i = 0;
@@ -25,44 +47,10 @@ int	ft_printf(const char *format, ...)
 		if (format[i] == '%' && format[i + 1] != '\0')
 		{
 			i++;
-			if (format[i] == 'c')
-			{
-				calc += ft_putchar(va_arg(args, int));
-			}
-			else if (format[i] == 's')
-			{
-				calc += ft_putstr(va_arg(args, char *));
-			}
-			else if (format[i] == 'p')
-			{
-				calc += ft_putp(va_arg(args, void *));
-			}
-			else if (format[i] == 'd' || format[i] == 'i')
-			{
-				calc += ft_putnbr(va_arg(args, int));
-			}
-			else if (format[i] == 'u')
-			{
-				calc += ft_putunbr(va_arg(args, unsigned int));
-			}
-			else if (format[i] == 'x')
-			{
-				calc += ft_phex(va_arg(args, unsigned int), 'x');
-			}
-			else if (format[i] == 'X')
-			{
-				calc += ft_phex(va_arg(args, unsigned int), 'X');
-			}
-			else if (format[i] == '%')
-			{
-				ft_print_pr();
-				calc++;
-			}
+			calc += h_specificateurs(format[i], args);
 		}
 		else
-		{
 			calc += write(1, &format[i], 1);
-		}
 		i++;
 	}
 	va_end(args);
